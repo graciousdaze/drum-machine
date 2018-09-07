3fc2afca657cc13b9c025eec4606491c13a2a892
Grace <graciousdaze@gmail.com>
2018-09-07 13:46:14 -0500

    Added on-click event to trigger sounds. Added function to trigger similar events for on-click and keydown event. Refactored keydown event to reduce redundancy.

--- scripts.js
+++ scripts.js
@@ -13,48 +13,22 @@ const sounds = {
 console.log("Connected!");
 
 $(document).keydown(function(event) {
-        var playSound = '';
-    if(event.which == 81){
-        playSound = sounds.q;
-    } else if (event.which == 87){
-        playSound = sounds.w;
-    } else if (event.which == 69){
-        playSound = sounds.e;
-    } else if (event.which == 65){
-        playSound = sounds.a;
-    } else if (event.which == 83){
-        playSound = sounds.s;
-    } else if (event.which == 68){
-        playSound = sounds.d;
-    } else if (event.which == 90){
-        playSound = sounds.z;
-    } else if (event.which == 88){
-        playSound = sounds.x;
-    } else if (event.which == 67){
-        playSound = sounds.c;
+    if(sounds[event.key] !== undefined){
+        playSound(sounds[event.key]);
     }
-    let makeNoise = new Howl({
-        src: playSound
-    });
-    makeNoise.play();
 });
 
-/* Add on-click logic
-// Refactor so the above code can be used for both onClick and KeyPress events
-// Maybe if(e.which == # || div.id == "letter")
-// Or...create a react/redux library?
-// Create Objects for Each Letter: 
-// var sounds = [
-    {
-        id: q,
-        key: 81,
-        src: 'sounds/squiggle.mp3'
-    },
-    {
-        id: w,
-        key: 87,
-        src: 'sounds/confetti.mp3'
-    }
-];
-//Put the HTML in the JS and map through each div, adding "OnClick" and "OnKeyPress" events 
-*/
+$(document).ready(function(){
+    $('.drum-pad').click(function(){
+        playSound(sounds[$(this).attr('id')]);
+    });
+});
+
+function playSound(soundSrc){
+    console.log(soundSrc);
+    let makeNoise = new Howl({
+        src: soundSrc
+    });
+    makeNoise.play();
+    console.log("La la la la");
+};
